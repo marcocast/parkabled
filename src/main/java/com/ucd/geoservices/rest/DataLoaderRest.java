153f59dadc90ba2dab3b74443bbea1eb50f98a8f
marcocast <marcocast@gmail.com>
2015-08-12 09:23:04 +0200

    adding dataloader

---
+++ src/main/java/com/ucd/geoservices/rest/DataLoaderRest.java
@@ -0,0 +1,41 @@
+package com.ucd.geoservices.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.aol.micro.server.auto.discovery.Rest;
+import com.ucd.geoservices.model.Location;
+import com.ucd.geoservices.service.LocationService;
+import com.ucd.geoservices.transformer.DubLinkedTransformer;
+
+
+@Path("/dataloader")
+@Component
+@Rest(isSingleton = true)
+public class DataLoaderRest {
+
+	@Autowired
+	private LocationService locationService;
+ 
+	@Autowired
+	private DubLinkedTransformer dubLinkedTransformer;
+
+	@POST
+	@Path("dublinked")
+	@Consumes("text/plain")
+	@Produces("application/json")
+	public Response anotherTest(final String csvData) {
+		List<Location> parkings = dubLinkedTransformer
+				.transformFromCSVData(csvData);
+		parkings.forEach(location -> locationService.addLocation(location));
+		return Response.ok("Data Loaded : " + parkings.size()).build();
+	}
+}
---
+++ src/main/java/com/ucd/geoservices/transformer/DubLinkedTransformer.java
@@ -0,0 +1,81 @@
+package com.ucd.geoservices.transformer;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.aol.cyclops.streams.Pair;
+import com.google.common.collect.Lists;
+import com.ucd.geoservices.geo.GeocoderService;
+import com.ucd.geoservices.model.Coordinates;
+import com.ucd.geoservices.model.Location;
+
+
+@Component
+public class DubLinkedTransformer {
+
+	@Autowired
+	private GeocoderService geocoderService;
+
+	public List<Location> transformFromCSVData(String csvData) {
+		List<Location> result = Lists.newArrayList();
+		Reader in = new StringReader(csvData);
+		CSVParser parser = null;
+		try {
+			parser = new CSVParser(in, CSVFormat.EXCEL);
+			parser.getRecords()
+					.stream()
+					.map(csvElement -> new Location(null, null,
+							"Ireland,Dublin " + csvElement.get(3) + " "
+									+ csvElement.get(0) + " "
+									+ csvElement.get(1), csvElement.get(2)))
+					.forEach(
+							location -> {
+
+								Optional<Pair<Double, Double>> longLat = geocoderService
+										.getLongLat(location.getName()
+												.replaceAll(" ", "+"));
+
+								longLat.ifPresent(pair -> {
+									result.add(location.withCoordinates(new Coordinates(
+											pair.getV1(), pair.getV2())));
+								});
+
+								result.stream().forEach(System.out::println);
+
+								Throttle();
+							});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (parser != null) {
+					parser.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+
+	}
+
+	private void Throttle() {
+		try {
+			Thread.sleep(300);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+}
